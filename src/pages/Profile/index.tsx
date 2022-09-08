eff872ec7f4a7ed8bdc062db22c2786dc12bfcd1
Patricia Dias Baldez <patriciabaldez1985@gmail.com>
2022-09-08 17:30:01 -0300

    styles

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Container } from './styles'
 
 const Profile = () => {
     return(
-        <div>Porfile</div>
+        <Container>
+            <Main>
+                <LeftSide></LeftSide>
+                <RightSide></RightSide>
+            </Main>
+        </Container>
     )
 }
 export default Profile
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,1 +1,10 @@
 import styled from 'styled-components';
+
+export const Container = styled.div``
+
+export const Main = styled.div`
+`
+export const LeftSide = styled.div`
+`
+export const RightSide =styled.div`
+`
